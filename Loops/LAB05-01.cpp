1f89b311405788e3c12c11c6f544311cdb22e03d
Teresa Scavotto <jamesscavotto.teresa@titans.easternflorida.edu>
2024-03-12 10:14:40 -0400

    Create counter controlled while loop
    Created the counter controlled while loop that stores the value and multiplies by 10 and 2

--- Loops/LAB05-01.cpp
+++ Loops/LAB05-01.cpp
@@ -20,6 +20,17 @@ int main()
    cout << "0 through 10 multiplied by 2 and by 10." << endl;
 
    // Write while loop here   
+      while (counter <= 10) {
+      int byTen = counter * 10;  // Stores the number multiplied by 10
+      int byTwo = counter * 2;  // Stores the number multiplied by 2
+    
+    // Loop body
+      cout << "Number: " << counter << endl;
+      cout << "Multiplied by 2: " << byTwo << endl;
+      cout << "Multiplied by 10: " << byTen << endl;
+      cout << endl;
+    counter++;  // Increment the loop control variable
+}
    
    return 0;
 } 
